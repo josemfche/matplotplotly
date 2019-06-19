3cc415b8b81a494aa0f864579b794cbcb434ebee
josemfche <che.manuel758@gmail.com>
2019-06-19 01:03:09 -0400

    Hola

---
+++ rw_visual.py
@@ -0,0 +1,16 @@
+import matplotlib.pyplot as plt
+from random_walk import RandomWalk
+
+# Make a random walk.
+
+rw = RandomWalk()
+rw.fill_walk()
+
+#Plot the points in the walk.
+plt.style.use('classic')
+fig, ax = plt.subplots()
+ax.scatter(rw.x_values, rw.y_values, s=10)
+
+plt.show()
+
+#355
--- scatter_squares.py
+++ scatter_squares.py
@@ -1,12 +1,12 @@
 import matplotlib.pyplot as plt
 
 
-x_values = range(1, 1001)
-y_values = [x**2 for x in x_values]
+x_values = range(1, 5001)
+y_values = [x**3 for x in x_values]
 
 plt.style.use('seaborn')
 fig, ax = plt.subplots()
-ax.scatter(x_values, y_values, s=10, c=y_values, cmap=plt.cm.Blues)
+ax.scatter(x_values, y_values, s=10, c='Red')
 
 
 #Set chart title and label axes.
@@ -16,12 +16,12 @@ ax.set_xlabel("Value", fontsize=14)
 ax.set_ylabel("Squares", fontsize=14)
 
 #Set the range for each axis.
-ax.axis([0, 1100, 0, 1100000])
+ax.axis([0, 11000, 0, 110000000000])
 
 
 #Set size of tick labels.
 ax.tick_params(axis='both', which='major', labelsize=14)
 
 
-plt.savefig('plot.png')
+plt.show()
 
