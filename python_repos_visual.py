4477f5c141653697349de2e15c46ac693e6da70e
unknown <che.manuel758@gmail.com>
2019-06-29 01:59:46 -0400

    Última modificación del libro

--- python_repos_visual.py
+++ python_repos_visual.py
@@ -13,36 +13,63 @@ print(f"Status code: {r.status_code}")
 # Process results.
 response_dict = r.json()
 #print(f"Total repositories: {response_dict['total_count']}")
+
 # Explore the information about the repositories.
 repo_dicts = response_dict['items']
-repo_names, stars = [], []
+repo_links, stars, labels = [], [], []
 print(f"Repositories returned: {len(repo_dicts)}")
 
-# Examine the first repository.
+# Examine each repository.
 print("\nSelected information about each repository:")
 for repo_dict in repo_dicts:
-    repo_names.append(repo_dict['name'])
+    repo_name = repo_dict['name']
+    repo_url = repo_dict['html_url']
+    repo_link = f"<a href='{repo_url}'>{repo_name}</a>"
+    repo_links.append(repo_link)
+
     stars.append(repo_dict['stargazers_count'])
+
+    owner = repo_dict['owner']['login']
+    description = repo_dict['description']
+    label = f"{owner}<br  />{description}"
+    labels.append(label)
 
 # Make visualization.
 data = [{
     'type': 'bar',
-    'x': repo_names,
+    'x': repo_links,
     'y': stars,
+    'hovertext': labels,
+    'marker': {
+        'color': 'rgb(60, 100, 150)',
+        'line': {'width': 1.5, 'color': 'rgb(25, 25, 25)'}
+    },
+    'opacity': 0.6,
 }]
 
 my_layout = {
-    'title': 'Most-Starred Python Projects on GitHub', 
-    'xaxis': {'title': 'Repository'},
-    'yaxis': {'title': 'Stars'},
+    'title': 'Most-Starred Python Projects on GitHub',
+    'titlefont': {'size': 28},
+    'xaxis': {
+        'title': 'Repository',
+        'titlefont': {'size': 24},
+        'tickfont': {'size': 14},
+    } ,
+    'yaxis': {
+        'title': 'Stars',
+        'titlefont': {'size': 24},
+        'tickfont': {'size': 14},
+    },
 }
 
 fig = {'data': data, 'layout': my_layout}
 offline.plot(fig, filename='python_repos.html')
 #print(f"\nKeys: {len(repo_dict)}")
+
 #for key in sorted(repo_dict.keys()):
 #    print(key)
 
 # Process results.
 #print(response_dict.keys())
 
+#405
